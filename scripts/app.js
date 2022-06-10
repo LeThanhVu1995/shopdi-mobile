55ebd36fde9530176bce896ffba9dfcd9a423bd8
LeThanhVu1995 <lethanhvu20015@gmail.com>
2022-06-10 10:58:45 +0700

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
-
 function scrollActive() {
   const scrollY = window.pageYOffset;
-  const navbar = document.querySelector('.navbar')
+  const navbar = document.querySelector(".navbar");
 
   if (scrollY > 65) {
     navbar.classList.add("fixed-menu");
@@ -10,11 +9,12 @@ function scrollActive() {
   }
 }
 
-
-const buttonDownload = document.getElementById('download-app')
-buttonDownload.addEventListener('click', function () {
-  window.scrollTo({ top: 0 })
-})
+const buttonDownload = document.getElementById("download-app");
+buttonDownload.addEventListener("click", function () {
+  window.location.replace(
+    "https://apps.apple.com/us/app/instagram/id389801252"
+  );
+  window.scrollTo({ top: 0 });
+});
 
 window.addEventListener("scroll", scrollActive);
-
