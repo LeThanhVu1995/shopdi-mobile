39c2c8a0cf174002a47153c9d6d7b2bd068beb7d
LeThanhVu1995 <lethanhvu20015@gmail.com>
2022-07-23 17:13:44 +0700

    update banner

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,24 @@
+var countDownDate = new Date("Aug 26, 2022 16:00:00").getTime();
+
+var x = setInterval(function () {
+  var now = new Date().getTime();
+
+  var distance = countDownDate - now;
+
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("countdown").innerHTML =
+    days + "D " + hours + "H " + minutes + "M " + seconds + "S ";
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
 function scrollActive() {
   const scrollY = window.pageYOffset;
   const navbar = document.querySelector(".navbar");
