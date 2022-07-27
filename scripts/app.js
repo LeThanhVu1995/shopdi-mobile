f7a1dbdb47a805808967e4691cdbfb020c874aca
LeThanhVu1995 <lethanhvu20015@gmail.com>
2022-07-27 09:05:56 +0700

    update

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Aug 26, 2022 16:00:00").getTime();
+var countDownDate = new Date("Aug 26, 2022 09:00:00").getTime();
 
 var x = setInterval(function () {
   var now = new Date().getTime();
@@ -26,23 +26,23 @@ var x = setInterval(function () {
   }
 }, 1000);
 
-function scrollActive() {
-  const scrollY = window.pageYOffset;
-  const navbar = document.querySelector(".navbar");
+// function scrollActive() {
+//   const scrollY = window.pageYOffset;
+//   const navbar = document.querySelector(".navbar");
 
-  if (scrollY > 65) {
-    navbar.classList.add("fixed-menu");
-  } else {
-    navbar.classList.remove("fixed-menu");
-  }
-}
+//   if (scrollY > 65) {
+//     navbar.classList.add("fixed-menu");
+//   } else {
+//     navbar.classList.remove("fixed-menu");
+//   }
+// }
 
-const buttonDownload = document.getElementById("download-app");
-buttonDownload.addEventListener("click", function () {
-  window.location.replace(
-    "https://apps.apple.com/us/app/instagram/id389801252"
-  );
-  window.scrollTo({ top: 0 });
-});
+// const buttonDownload = document.getElementById("download-app");
+// buttonDownload.addEventListener("click", function () {
+//   window.location.replace(
+//     "https://apps.apple.com/us/app/instagram/id389801252"
+//   );
+//   window.scrollTo({ top: 0 });
+// });
 
-window.addEventListener("scroll", scrollActive);
+// window.addEventListener("scroll", scrollActive);
